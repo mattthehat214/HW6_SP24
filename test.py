e1650a1302da9c0cda7525d42422b3e200eee5b6
Ryanellis21 <157422052+Ryanellis21@users.noreply.github.com>
2024-03-11 11:28:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from rankine import rankine
 
+
 def main():
     '''
     A test program for rankine power cycles.
@@ -7,16 +8,20 @@ def main():
     R2 is a rankine cycle object that is instantiated for turbine inlet of superheated vapor.
     :return: none
     '''
-    R1=rankine(p_high=8000, p_low=8, name='Rankine cycle - saturated steam inlet')
+    R1 = rankine(p_high=8000, p_low=8, name='Rankine cycle - saturated steam inlet')
+    #This line creates an instance of the rankine class with the specified parameters for a turbine inlet of saturated vapor.
     R1.calc_efficiency()
+    #This line calculates the efficiency of the rankine cycle represented by the R1 object.
 
-    Tsat=R1.state1.T
-    R2=rankine(p_high=8000, p_low=8, t_high=1.7*Tsat, name='Rankine cycle - superheated steam inlet')
+    Tsat = R1.state1.T
+    # This line assigns the temperature of the saturated vapor at the turbine inlet to the variable Tsat
+    R2 = rankine(p_high=8000, p_low=8, t_high=1.7 * Tsat, name='Rankine cycle - superheated steam inlet')
     R2.calc_efficiency()
+    #This line calculates the efficiency of the rankine cycle represented by the R2 object.
 
     R1.print_summary()
     R2.print_summary()
 
-    
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
