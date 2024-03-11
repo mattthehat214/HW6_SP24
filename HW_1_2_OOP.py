c016c2d3705c0279a3b35965b4514b088b426000
josebastidas7 <157265279+josebastidas7@users.noreply.github.com>
2024-03-11 12:28:15 -0500

    Create HW_1_2_OOP.py

---
+++ HW_1_2_OOP.py
@@ -0,0 +1,45 @@
+from HW6_1_OOP import ResistorNetwork, Resistor, VoltageSource, Loop
+from scipy.optimize import fsolve
+
+class ResistorNetwork2(ResistorNetwork):
+    def __init__(self):
+        super().__init__()
+        self.Loops = []  
+        self.Resistors = []  
+        self.VSources = []  
+
+    def AnalyzeCircuit(self):
+        currents_initial_guess = [1]*len(self.Resistors)
+        currents, info, ier, msg = fsolve(self.GetKirchoffVals, currents_initial_guess, full_output=True)
+        if ier != 1:
+            print(f"fsolve did not converge, message: {msg}")
+        else:
+            for idx, current in enumerate(currents, start=1):
+                print(f"I{idx} = {current:.1f}A")
+        return currents
+
+    def GetKirchoffVals(self, currents):
+        self.GetResistorByName('ad').Current = currents[0]
+        self.GetResistorByName('bc').Current = currents[0]
+        self.GetResistorByName('ce').Current = currents[4]
+        self.GetResistorByName('de').Current = currents[3]
+        self.GetResistorByName('cd').Current = currents[2]
+
+        Node_c_Current = sum([currents[4], -currents[2], currents[0]])
+        Node_d_Current = sum([-currents[1], currents[2], -currents[0], currents[3]])
+
+        KVL = self.GetLoopVoltageDrops()
+        KVL.append(Node_c_Current)
+        KVL.append(Node_d_Current)
+
+        return KVL
+
+def main():
+    net = ResistorNetwork2()
+    net.BuildNetworkFromFile('ResistorNetwork_2.txt')
+    currents = net.AnalyzeCircuit()
+    print(f"Calculated currents for 1-5 : {[f'{current:.1f}A' for current in currents]}")
+
+if __name__ == "__main__":
+    main()
+  
